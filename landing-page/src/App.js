4c82bb68e10118f69e413e2cf1a0101d5199b912
jcarr98 <jeffrey.carr98@gmail.com>
2019-10-03 14:06:20 -0400

    Created landing page React app

---
+++ landing-page/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = "Ultimate Project Landing Page";
+    return (
+      <div>Welcome to the {name}</div>
+    );
+  }
+}
+
+export default App;
