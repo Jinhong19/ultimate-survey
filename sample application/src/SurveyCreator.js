3e8b3dc133b3d7d58dcca52ee88a4369c4d57195
Peilan Wang <lw980627@gmail.com>
2019-10-09 21:42:42 -0400

    Added react sample application files

---
+++ sample application/src/SurveyCreator.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import * as SurveyJSCreator from "survey-creator";
+import * as SurveyKo from "survey-knockout";
+import "survey-creator/survey-creator.css";
+
+import "jquery-ui/themes/base/all.css";
+import "nouislider/distribute/nouislider.css";
+import "select2/dist/css/select2.css";
+import "bootstrap-slider/dist/css/bootstrap-slider.css";
+
+import "jquery-bar-rating/dist/themes/css-stars.css";
+import "jquery-bar-rating/dist/themes/fontawesome-stars.css";
+
+import $ from "jquery";
+import "jquery-ui/ui/widgets/datepicker.js";
+import "select2/dist/js/select2.js";
+import "jquery-bar-rating";
+
+import "icheck/skins/square/blue.css";
+
+import * as widgets from "surveyjs-widgets";
+
+SurveyJSCreator.StylesManager.applyTheme("default");
+
+widgets.icheck(SurveyKo, $);
+widgets.select2(SurveyKo, $);
+widgets.inputmask(SurveyKo);
+widgets.jquerybarrating(SurveyKo, $);
+widgets.jqueryuidatepicker(SurveyKo, $);
+widgets.nouislider(SurveyKo);
+widgets.select2tagbox(SurveyKo, $);
+widgets.signaturepad(SurveyKo);
+widgets.sortablejs(SurveyKo);
+widgets.ckeditor(SurveyKo);
+widgets.autocomplete(SurveyKo, $);
+widgets.bootstrapslider(SurveyKo);
+
+class SurveyCreator extends Component {
+  surveyCreator;
+  componentDidMount() {
+    let options = { showEmbededSurveyTab: true };
+    this.surveyCreator = new SurveyJSCreator.SurveyCreator(
+      "surveyCreatorContainer",
+      options
+    );
+    this.surveyCreator.saveSurveyFunc = this.saveMySurvey;
+  }
+  render() {
+    return <div id="surveyCreatorContainer" />;
+  }
+  saveMySurvey = () => {
+    console.log(JSON.stringify(this.surveyCreator.text));
+  };
+}
+
+export default SurveyCreator;
