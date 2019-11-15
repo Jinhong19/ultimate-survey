c6d1591f006b38d0fd2fdfc71c6f2a027e5e9b81
jcarr98 <jjcarr@umas.edu>
2019-11-15 18:02:08 -0500

    changed some padding changes to margin changes for simplicity

--- FRONTEND/src/Components/LoginCard.js
+++ FRONTEND/src/Components/LoginCard.js
@@ -4,18 +4,20 @@ import { withStyles, Card, CardContent, CardActions, Button, TextField,
 
 const styles = theme => ({
   text: {
-    padding: '1em',
-    paddingTop: '2em',
-    paddingBottom: '0em',
+    margin: '1em',
+    marginTop: '2em',
+    marginBottom: '0em',
   },
   card: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '50px',
     minHeight: '21em',
     width: '40em',
-    marginTop: "50px",
     textAlign: 'center'
   },
   entry: {
-    paddingTop: "0.7em",
+    marginTop: "0.7em",
   },
   password: {
       marginLeft: 'auto',
@@ -30,56 +32,54 @@ class LoginCard extends React.Component {
 
     render() {
         return (
-            <Grid item>
-                <Card className={this.props.classes.card}>
-                    <CardContent>
-                        <Typography align="center" component="h4" variant="h4">
-                            {this.props.user}
-                        </Typography>
-                        <Divider />
-                        <Typography
-                            className={this.props.classes.text}
-                            align="center"
-                            component="p"
-                            variant="p"
-                        >
-                            Please login to be directed to your survey center.
-                        </Typography>
-                        <TextField
-                            className={this.props.classes.entry}
-                            label="Username"
-                            variant="outlined"
-                            fullWidth="true"
-                        />
-                        <TextField
-                            className={this.props.classes.entry}
-                            label="Password"
-                            variant="outlined"
-                            type="password"
-                            fullWidth="true"
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            fullWidth="true"
-                            href={this.props.redir}
-                        >
-                            <Typography color="secondary">Login</Typography>
-                        </Button>
-                    </CardActions>
-                    <CardActions>
-                    <Button 
-                        className={this.props.classes.password}
-                        size="small"
-                        href="/forgot"
+            <Card className={this.props.classes.card}>
+                <CardContent>
+                    <Typography align="center" component="h4" variant="h4">
+                        {this.props.user}
+                    </Typography>
+                    <Divider />
+                    <Typography
+                        className={this.props.classes.text}
+                        align="center"
+                        component="p"
+                        variant="p"
                     >
-                        Forgot password
+                        Please login to be directed to your survey center.
+                    </Typography>
+                    <TextField
+                        className={this.props.classes.entry}
+                        label="Username"
+                        variant="outlined"
+                        fullWidth="true"
+                    />
+                    <TextField
+                        className={this.props.classes.entry}
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                        fullWidth="true"
+                    />
+                </CardContent>
+                <CardActions>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        fullWidth="true"
+                        href={this.props.redir}
+                    >
+                        <Typography color="secondary">Login</Typography>
                     </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
+                </CardActions>
+                <CardActions>
+                <Button 
+                    className={this.props.classes.password}
+                    size="small"
+                    href="/forgot"
+                >
+                    Forgot password
+                </Button>
+                </CardActions>
+            </Card>
         );
     }
 }
