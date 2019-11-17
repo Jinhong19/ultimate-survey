4c6a9c46b6d1783551e44ba57fde60db76b7d4db
jcarr98 <jjcarr@umas.edu>
2019-11-17 16:02:34 -0500

    Finished forgot password page UI, no backend connection yet

--- FRONTEND/src/Components/ForgotCard.js
+++ FRONTEND/src/Components/ForgotCard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { withStyles, Button, Card, CardHeader, CardContent, CardActions, 
             TextField, Typography } from '@material-ui/core';
+import ForgotPre from './ForgotPre.js';
+import ForgotPost from './ForgotPost';
 
 const styles = theme => ({
     container: {
@@ -14,17 +16,6 @@ const styles = theme => ({
         width: '40em',
         textAlign: 'center'
     },
-    text: {
-        margin: '1em',
-        marginBottom: '0em',
-        marginBottom: '0'
-    },
-    entry: {
-        marginTop: '0.7em'
-    },
-    submit: {
-        color: 'white'
-    },
     back: {
         marginLeft: 'auto',
         marginRight: 'auto'
@@ -32,37 +23,39 @@ const styles = theme => ({
 });
 
 class ForgotCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            done: <ForgotPre invalid={false} sub={this.onSubmit.bind(this)} onUpdate={this.updateEmail.bind(this)} />, 
+            email: ''
+        };
     }
 
-    render(){
+    updateEmail = newEmail => {
+        this.setState({
+            email: newEmail.target.value
+        });
+    }
+
+    onSubmit = () => {
+        if(this.state.email.length < 1){
+            this.setState({
+                done: <ForgotPre invalid={true} sub={this.onSubmit.bind(this)} onUpdate={this.updateEmail.bind(this)} />
+            })
+        } 
+        else {
+            this.setState({
+                done: <ForgotPost />
+            });
+        }
+    };
+
+    render() {
         return(
             <div className={this.props.classes.container} >
                 <Card className={this.props.classes.card} >
                     <CardHeader title="Password Reset" />
-                    <CardContent>
-                        <Typography className={this.props.classes.text} variant="p" >
-                            Enter your email below to reset your password
-                        </Typography>
-                        <TextField 
-                            required
-                            className={this.props.classes.entry}
-                            label="Email"
-                            fullWidth="true"
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            className={this.props.classes.submit}
-                            variant="contained"
-                            color="primary"
-                            fullWidth="true"
-                            href="#"
-                        >
-                            Submit
-                        </Button>
-                    </CardActions>
+                    {this.state.done}
                     <CardActions>
                         <Button className={this.props.classes.back} href="/">Back to login page</Button>
                     </CardActions>
