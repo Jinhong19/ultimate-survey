01fd6ec34ed6043493e5cd7272ea169112e27cea
PeilanWang <lw980627@gmail.com>
2019-11-20 00:03:57 -0500

    login button function in loggincard

--- FRONTEND/src/Components/LoginCard.js
+++ FRONTEND/src/Components/LoginCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles, Card, CardContent, CardActions, Button, TextField,
             Typography, Divider, Grid } from '@material-ui/core';
-
+import axios from 'axios'
 const styles = theme => ({
   text: {
     margin: '1em',
@@ -28,7 +28,38 @@ const styles = theme => ({
 class LoginCard extends React.Component {
     constructor(props) {
         super(props);
+		this.state = {
+        email: "",
+        password: "",
+        };
+        this.handleChange_password = this.handleChange_password.bind(this);
+        this.handleChange_username = this.handleChange_username.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+	
+	handleChange_username(event){
+    this.setState({username: event.target.value});
+    }
+
+    handleChange_password(event){
+    this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event){
+    event.preventDefault();
+    const username = this.state.username;
+    const password = this.state.password;
+	//TODO uncommon next two line can check the value being submitted to the api call
+	//console.log(username)
+	//console.log(password)
+    axios.post("https://ultimate-survey.herokuapp.com/login", this.state)
+    .then(res => console.log(res))
+    }
+
+//    handleClick(){
+//    axios.get("http://127.0.0.1:5000/login")
+//    .then(response => this.setState({result: response}))
+//    }
 
     render() {
         return (
@@ -50,12 +81,14 @@ class LoginCard extends React.Component {
                         className={this.props.classes.entry}
                         label="Username"
                         fullWidth="true"
+						onChange = {this.handleChange_username}
                     />
                     <TextField
                         className={this.props.classes.entry}
                         label="Password"
                         type="password"
                         fullWidth="true"
+						onChange = {this.handleChange_password}
                     />
                 </CardContent>
                 <CardActions>
@@ -64,6 +97,7 @@ class LoginCard extends React.Component {
                         color="primary"
                         fullWidth="true"
                         href={this.props.redir}
+						onClick={this.handleSubmit}
                     >
                         <Typography color="secondary">Login</Typography>
                     </Button>
