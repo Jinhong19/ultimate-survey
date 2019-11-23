1158b5c1a52a33a26e2e8a98d5554e931836c89e
jcarr98 <jjcarr@umas.edu>
2019-11-23 01:34:53 -0500

    Created change password page placeholder and linked with nav bar

---
+++ FRONTEND/src/Pages/ChangePassword.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ChangePassword extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>This is where the change password page will be.</h1>
+                <a href="/dashboard">Back to dashboard</a>
+            </div>
+        );
+    }
+}
+
+export default ChangePassword;
