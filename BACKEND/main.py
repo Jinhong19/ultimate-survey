b98ef0d1cb7868d7a74ea9975925170be95fe846
PeilanWang <lw980627@gmail.com>
2019-11-28 01:04:10 -0500

    Hashing password, and resetPassword function

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -8,63 +8,71 @@ from bson import json_util
 from bson.json_util import dumps, loads  # used to convert Python MongoDB JSON to/from BSON
 from flask import Flask, Response, redirect, url_for, request, session, abort
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from passlib.hash import sha256_crypt
 
-#Flask App
+# Flask App
 app = flask.Flask("__main__")
 app.config.update(
     DEBUG=True,
     SECRET_KEY='the_biggest_secret'
 )
 
-#DB connection
-mongo = PyMongo(app, uri='mongodb+srv://testUser:testUserUltimate3@ultimatesurvey-74jff.mongodb.net/Platform?retryWrites=true&w=majority')
+# DB connection
+mongo = PyMongo(app,
+                uri='mongodb+srv://testUser:testUserUltimate3@ultimatesurvey-74jff.mongodb.net/Platform?retryWrites=true&w=majority')
 
 # flask-login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
 class User(UserMixin):
     def __init__(self, person):
         self.username = person['email']
         self._id = str(person['_id'])
-        self.isManager = True #TODO - grab attribute from database once implemented
+        self.isManager = True  # TODO - grab attribute from database once implemented
         self.fname = person['firstName']
         self.lname = person['lastName']
+
     def get_id(self):
         return self.username
+
     def get_objectid(self):
         return self._id
 
+
 # AUTHENTICATION -------------------------------------------------------
 
+# Grab the hashed password from database (in order to change the normal pw to hashed-pw, use /resetPassword,
+# type in any password, it will be hashed and stored)
+# Hash user input password, then compare password
 @app.route("/login", methods=["GET", "POST"])
 @cross_origin(supports_credentials=True)
 def login():
     users = mongo.db.Employees
     person = users.find_one({'email': request.get_json(force=True)['username']})
     if person:
-    # hashed = bcrypt.hashpw(request.json['password'].encode('utf-8'), bcrypt.gensalt())
-    # if bcrypt.checkpw(login_user['password'].encode('utf-8'), hashed):
-    #     session['email'] = request.json['email']
-    #     return 'You in'
-        if person['password'] == request.get_json(force=True)['password']:
+        if sha256_crypt.verify(request.get_json(force=True)['password'], person['password']):
             user_obj = User(person)
             login_user(user_obj)
-            return flask.jsonify({'message':'success', 'isManager':user_obj.isManager, 'fname':user_obj.fname, 'lname':user_obj.lname})
+            return flask.jsonify({'message': 'success', 'isManager': user_obj.isManager, 'fname': user_obj.fname,
+                                  'lname': user_obj.lname})
+    return flask.jsonify({'message': "failure"})
 
-    return flask.jsonify({'message':"failure"})
 
 @app.route('/logout')
 @login_required
 @cross_origin(supports_credentials=True)
 def logout():
     logout_user()
-    return flask.jsonify({'message':'success'})
+    return flask.jsonify({'message': 'success'})
+
 
 @app.errorhandler(401)
 def page_not_found():
     return Response('<p>Login failed</p>')
+
 
 # callback to reload the user object
 @login_manager.user_loader
@@ -75,27 +83,44 @@ def load_user(username):
     return User(user)
 
 
+# reset password, and store hashed password in the database
+@app.route('/resetPassword', methods=['POST'])
+def update_password():
+    users = mongo.db.Employees
+    body = request.get_json(force=True)
+    user = users.find_one({"email": body["username"]})
+    if not user:
+        return flask.jsonify({"message": "Username does not exist."})
+    elif not body["password"]:
+        return flask.jsonify({"message": "Password can not be empty."})
+    else:
+        hashed_pw = sha256_crypt.encrypt(body["password"])
+        users.update({"email": body["username"]}, {"$set": {"password": hashed_pw}}, upsert=False)
+        return flask.jsonify({'message': "Reset Password Success!"})
+
+
 # EMPLOYEE ---------------------------------------------------------------- 
 
-#GET - return responses for a certain employeeid
-#POST - pushes a survey response to the database
+# GET - return responses for a certain employeeid
+# POST - pushes a survey response to the database
 @app.route('/response', methods=['GET', 'POST'])
 @login_required
 @cross_origin(supports_credentials=True)
 def user_response():
-	if flask.request.method == 'GET':
-		responses = mongo.db.Responses
-		cursor_query= responses.find({"employeeid":ObjectId(current_user._id)})
-		return flask.jsonify(dumps(list(cursor_query)))
+    if flask.request.method == 'GET':
+        responses = mongo.db.Responses
+        cursor_query = responses.find({"employeeid": ObjectId(current_user._id)})
+        return flask.jsonify(dumps(list(cursor_query)))
 
-	elif flask.request.method == 'POST':
-		responses = mongo.db.Responses
-		body = request.get_json(force=True)
-		_employeeid = ObjectId(current_user._id)
-		object_id = responses.insert_one({'surveyid': 'SomeSurveyid', 'response':body, 'employeeid':_employeeid})
-		return flask.jsonify({'message':"Inserted Response for Employee " + str(current_user._id)})
+    elif flask.request.method == 'POST':
+        responses = mongo.db.Responses
+        body = request.get_json(force=True)
+        _employeeid = ObjectId(current_user._id)
+        object_id = responses.insert_one({'surveyid': 'SomeSurveyid', 'response': body, 'employeeid': _employeeid})
+        return flask.jsonify({'message': "Inserted Response for Employee " + str(current_user._id)})
 
-#GET - return a list of all surveys available to a employee
+
+# GET - return a list of all surveys available to a employee
 @app.route('/survey/employee', methods=['GET'])
 @cross_origin(supports_credentials=True)
 @login_required
@@ -108,15 +133,15 @@ def user_survey():
 
 # MANAGER ----------------------------------------------------------------
 
-#GET - return a list of all surveys created by a manager
-#POST - submit a new survey - survey body is not modified - also performs query of employees under manger
+# GET - return a list of all surveys created by a manager
+# POST - submit a new survey - survey body is not modified - also performs query of employees under manger
 @app.route('/survey/manager', methods=['GET', 'POST'])
 @cross_origin(supports_credentials=True)
 @login_required
 def get_created_surveys():
     if flask.request.method == 'GET':
         surveys = mongo.db.Surveys
-        cursor_query = surveys.find({"manager": ObjectId(current_user._id)})  
+        cursor_query = surveys.find({"manager": ObjectId(current_user._id)})
         return flask.jsonify(dumps(list(cursor_query)))
     else:
         surveys = mongo.db.Surveys
@@ -124,30 +149,30 @@ def get_created_surveys():
         to_send = {'survey': body, 'manager': ObjectId(current_user._id),
                    'Employees': userDFS(current_user._id)}
         object_id = surveys.insert(to_send)
-        return flask.jsonify({'message':"Inserted survey for manger: " + str(current_user._id)})
+        return flask.jsonify({'message': "Inserted survey for manger: " + str(current_user._id)})
 
 
 def userDFS(manager_id):
-	employees = mongo.db.Employees
-	query = {'_id': ObjectId(manager_id)}
-	manager_data = employees.find(query)
-	manager_count = employees.count_documents(query)
+    employees = mongo.db.Employees
+    query = {'_id': ObjectId(manager_id)}
+    manager_data = employees.find(query)
+    manager_count = employees.count_documents(query)
 
-	result = []
-	if manager_count > 0:
-		entry = manager_data.next()
-		manager_employeeid = entry['employeeId']
+    result = []
+    if manager_count > 0:
+        entry = manager_data.next()
+        manager_employeeid = entry['employeeId']
 
-		query = {'managerId': manager_employeeid}
-		employees_of = employees.find(query)
-		count_employees_of = employees.count_documents(query)
-		
-		for doc in employees_of:
-			user_employees = userDFS(doc['_id'])
-			result.append(ObjectId(doc['_id']))
-			for user in user_employees:
-				result.append(user)
-	return result
+        query = {'managerId': manager_employeeid}
+        employees_of = employees.find(query)
+        count_employees_of = employees.count_documents(query)
+
+        for doc in employees_of:
+            user_employees = userDFS(doc['_id'])
+            result.append(ObjectId(doc['_id']))
+            for user in user_employees:
+                result.append(user)
+    return result
 
 
 @app.route('/responses/<survey_id>', methods=['GET'])
