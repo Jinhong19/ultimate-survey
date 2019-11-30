302d91492c23268bccf6256338446e7d28371f0e
jcarr98 <jjcarr@umas.edu>
2019-11-29 23:34:53 -0500

    Started modifying take survey page and used for snackbar testing

--- FRONTEND/src/Pages/TakeSurvey.js
+++ FRONTEND/src/Pages/TakeSurvey.js
@@ -1,31 +1,45 @@
 import React from 'react';
+import Link from 'react-router-dom/Link';
 import { Button } from 'react-bootstrap';
 
 class TakeSurvey extends React.Component {
     constructor(props){
         super(props);
-        this.state = {surveyID: "0000"};
+        this.state = {surveyID: "0000", surveyName: ""};
         this.updateState = this.updateState.bind(this);
     }
 
     componentDidMount = () => {
-        let receivedID = this.props.location.state.surveyID;
-        this.updateState(receivedID);
+        if(this.props.location.state === undefined){
+            this.updateState("BAD");
+        }
+        else {
+            let receivedID = this.props.location.state.surveyID;
+            this.updateState(receivedID);
+        }
     }
 
     updateState(prevState) {
         this.setState({
-            surveyID: prevState
+            surveyID: prevState,
+            surveyName: "Survey " + prevState // for testing
         });
     }
 
     render() {
+        if(this.state.surveyID === "BAD"){  // This should only trigger if user types in the URL rather than picking a survey
+            return(
+                <div style={{textAlign: "center"}}>
+                    <h1>ERROR: PLEASE PICK A SURVEY</h1>
+                    <a href="/dashboard">Back to dashboard</a>
+                </div>
+            );
+        }
         return(
             <div style={{textAlign: "center"}}>
-                <Button style={{float: "left", margin: "0.7em", marginTop: "0em"}} href="/dashboard">Back to dashboard</Button>
-                <h1>{this.props.surveyName}</h1>
-                <p>The survey you clicked: {this.state.surveyID}</p>
-                <Link to={{pathname: "/dashboard", state: {submit: true}}}><Button>Submit</Button></Link>
+                <Link to={{pathname: "/dashboard", state: {submit: false}}}>Back to Dashboard</Link>
+                <h1>{this.state.surveyName}</h1>
+                <Link to={{pathname: "/dashboard", state: {submit: true}}}>Submit</Link>
             </div>
         );
     }
