c5aac34801e7260b2f15f013bbe2f4fd01c24bbe
Gwyneth Thomas <gwyneththoma@umass.edu>
2019-12-02 23:34:55 -0500

    fixed displaying surveys

--- FRONTEND/src/Components/EmployeeDisplaySurvey.js
+++ FRONTEND/src/Components/EmployeeDisplaySurvey.js
@@ -1,8 +1,5 @@
 import React, {Component} from "react";
-import {TableCell, TableRow} from '@material-ui/core';
-import Survey from '../Components/Survey';
-import SurveyMenu from '../Components/SurveyMenu';
-import SurveyTakeButton from '../Components/SurveyTakeButton';
+import DashboardComponent from "../Components/DashboardComponent";
 
 class EmployeeDisplaySurvey extends React.Component{
 	constructor(props){
@@ -11,20 +8,6 @@ class EmployeeDisplaySurvey extends React.Component{
 			surveys: [],
 			pastDate: false
 		};
-		this.displaySurvey = this.displaySurvey.bind(this);
-		this.numSurveys=0;
-		this.count=0;
-	}
-
-	displaySurvey = (survey) =>{
-		<div>
-			<TableRow>
-				<TableCell align="right">survey.Manager</TableCell>
-				<TableCell align = "right">survey.exDate</TableCell>
-				<TableCell align = "right">this.state.pastDate</TableCell>
-			</TableRow>
-			<SurveyTakeButton button= {{json: survey}}></SurveyTakeButton>
-		</div>
 	}
 	componentDidMount(){
 		fetch("https://ultimate-survey.herokuapp.com//survey/employee", 
@@ -37,23 +20,17 @@ class EmployeeDisplaySurvey extends React.Component{
 			.then(data => <div>this.setState({this.state.surveys.push(data)})</div>);
 	}
 
+	getSurveys=()=>{
+		for(let i=0; i<=this.state.surveys.length; i++){
+			console.log("in for loop");
+			<DashboardComponent component = {this.state.surveys, i, this.state.pastDate}></DashboardComponent>
+		}
+	}
 	render(){
-		if(this.state.survey != undefined){
-			var numSurveys = this.state.survey.length;
-			var count = 0;
-		}
-		else{
-			var numSurveys = 0;
-			var count = 0;
-		}
 		return(
-			getSurveys=>{
-				while(numSurveys > count) {
-					<div>
-						displaySurvey(this.state.surveys[count]);
-					</div>
-				}
-			}	
+			<div>
+				{this.getSurveys()}
+			</div>
 		);
 	}
 }
